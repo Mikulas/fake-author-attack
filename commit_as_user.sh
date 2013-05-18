b3ad8221dc5349170138ba059987d9c451294409
Mikulas <fabien@symfony.com>
2013-05-18 15:31:50 +0200

    rewrote fake license

--- commit_as_user.sh
+++ commit_as_user.sh
@@ -2,5 +2,5 @@
 
 git config --global user.email $1
 git config --global --list | grep email > git_config
-git commit -am "commiting as $1"
-git push github master
+#git commit -am "commiting as $1"
+#git push github master
